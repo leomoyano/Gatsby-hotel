e39182391aafe97a235fdb03a5ac3aee66a58f96
unknown <leomoyano7@gmail.com>
2020-07-18 17:54:22 -0300

    Avanzando

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,14 +8,34 @@ export const Layout = (props) => {
         <>
             <Global
                 styles={css`
+                html{
+                    font-size: 62.5%;
+                }
                 body {
-                    background-color: blue;
+                    font-size: 15px;
+                    font-size: 1.6rem;
+                    line-height: 1.5;
                     }
+                 h1,h2,h3 {
+                     margin: 0;
+                     line-height: 1.5;
+                 } 
+                 h1,h2 {
+                     font-family: 'Roboto', serif;
+                 }  
+                 h3 {
+                    font-family: 'PT Sans', sans-serif;
+                 }
+                 ul {
+                     list-style: none;
+                     margin: 0;
+                     padding: 0;
+                 }
                 `}
             />
             <Helmet>
                 <title>Leo Moyano Dev</title>
-                <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" />
+                <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" rel="stylesheet"/>
                 <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
             </Helmet>
             <Header />
