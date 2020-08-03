5e02b3e6836f12b86cf112cd6b344ed41209de58
unknown <leomoyano7@gmail.com>
2020-08-03 02:06:21 -0300

    Add habitaciones data

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,21 @@ import React from "react";
 import Layout from "../components/layout";
 import ImagenHotel from "../components/imagenHotel";
 import ContenidoInicio from "../components/contenidoInicio";
+import useHabitaciones from "../hooks/use-habitaciones";
 
 
-const Index = () => (
-  <Layout>
+const Index = () => {
+  
+  const habitaciones = useHabitaciones();
+
+  return (
+
+    <Layout>
     <ImagenHotel />
     <ContenidoInicio />
   </Layout>
-)
+
+  )
+}
 
 export default Index
